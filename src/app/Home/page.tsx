cf5d9df83dff362a21d6b70a7f8aac3b504c40db
biagio360 <facundobiagioni@gmail.com>
2025-02-13 19:14:41 -0300

    Primer commit con configuración inicial

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">¡Hola, mundo! 🚀</h1>
+      </div>
+    );
+  }
+  
---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        miFuente: ['Bogart', 'sans-serif'],
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        'custom-purple': '#b093bf',
+        'custom-orange': '#d57159',
+        'valkyrie-purple': '#7b548b',
+        'creativity-purple': '#b093bf',
+        'purple-dark': '#3e1a4d'
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
