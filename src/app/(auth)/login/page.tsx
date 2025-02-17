f81f17da213f9c997302592275b5657dfb048a39
ITAlonsoRabanal <nachoalonso273@gmail.com>
2025-02-17 19:42:12 -0300

    Primer fix commit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Login() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-gray-100">
-            <h1 className="text-3xl font-bold text-blue-600">Hola mundos 2.0</h1>
+            <h1 className="text-3xl font-bold text-blue-600">Hola mundosss 2.0</h1>
         </div>
     );
 }
