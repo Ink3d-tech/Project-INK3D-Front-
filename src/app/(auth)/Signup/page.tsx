2aec7284a8efffc928a599cf20390b0f6c40b27d
Ignacio Tomas Alonso Rabanal <nachoalonso273@gmail.com>
2025-02-18 15:53:34 -0300

    Delete src/app/(auth)/Signup directory

--- src/app/(auth)/Signup/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Signup() {
-    return (
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <h1 className="text-3xl font-bold text-blue-600">Hola mundos 4.0</h1>
-      </div>
-    );
-  }
-  
